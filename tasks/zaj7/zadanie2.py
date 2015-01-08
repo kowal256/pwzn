ca48b4b7dbc7fe246d1c9d9d31949a13dba5c473
Daniel Kowalski <kowal256@gmail.com>
2015-01-08 05:45:30 +0100

    no way, i give up -> tomorrow

---
+++ tasks/zaj7/zadanie2.py
@@ -0,0 +1,41 @@
+__author__ = 'dk'
+
+import requests
+import argparse
+from math import ceil
+from multiprocessing import Pool, Queue, Process
+import hashlib
+import bs4
+
+
+def get_links(page):
+    if page[1] >= 5:
+        return []
+    else:
+
+
+def crawl(session, visited, to_go):
+    while True:
+        page = to_go.get()
+
+        for link in get_links(page):
+            visited.put(link)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('host', help='host')
+    parser.add_argument('port')
+    parser.add_argument('N', help='Number of processes')
+
+    args = parser.parse_args()
+
+    s = requests.Session()
+    s.post("{}/{}/login".format(args.host, args.port), {'user': 'foo', 'password': 'bar'})
+
+    visited, to_go = Queue(), Queue()
+    to_go.put((0, "{}:{}/12345niepamietam".format(args.host, args.port)))
+
+    #
+
+
